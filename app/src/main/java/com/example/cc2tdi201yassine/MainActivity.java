05b10643240b13084b71f469499731fd90554af9
yassinaoulad <aouladyassin@gmail.com>
2022-06-03 16:38:27 +0100

    Q7

--- app/src/main/java/com/example/cc2tdi201yassine/MainActivity.java
+++ app/src/main/java/com/example/cc2tdi201yassine/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cc2tdi201yassine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnMainActClick(View view) {
+        Intent i=null;
+        switch(view.getId()){
+            case R.id.btnAjoSoc: i=new Intent(this,ajouter_soc.class);break;
+            case R.id.btnEditSic: i=new Intent(this,editer_soc.class);break;
+            case R.id.btnListSoc: i=new Intent(this,listcos.class);break;
+        }
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/cc2tdi201yassine/listcos.java
@@ -0,0 +1,14 @@
+package com.example.cc2tdi201yassine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listcos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listcos);
+    }
+}
