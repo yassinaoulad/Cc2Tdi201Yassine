3694c6bba4fe2e67a36a510b47622ae051da386a
yassinaoulad <aouladyassin@gmail.com>
2022-06-03 15:47:27 +0100

    Q4

--- app/src/main/java/com/example/cc2tdi201yassine/MyDB.java
+++ app/src/main/java/com/example/cc2tdi201yassine/MyDB.java
@@ -1,4 +1,29 @@
 package com.example.cc2tdi201yassine;
 
-public class MyDB {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDB extends SQLiteOpenHelper {
+    public static String dbName="dbSociete.db";
+    public static String TABLE_NAME="societe";
+    public static String col1="ID";
+    public static String col2="Raison_Sociale";
+    public static String col3="Secteur_activite";
+    public static String col4="nb_employe";
+
+    public MyDB(Context context){super(context,dbName,null,1);}
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql = "create table " + TABLE_NAME + "(" +col1+" integer primary key autoincrement,"+col2+" TEXT,"+col3+" TEXT,"+col4+" integer)";
+        sqLiteDatabase.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String sql= "DROP TABLE " + TABLE_NAME;
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
+    }
 }
